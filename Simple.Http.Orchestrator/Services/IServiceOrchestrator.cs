825036addc413877659acdee59f97e53f781eb5e
Arnas Golubickas <arnasgo@gmail.com>
2023-08-24 23:48:59 +0300

    Remove additional folders

--- Simple.Http.Orchestrator/Services/IServiceOrchestrator.cs
+++
@@ -1,8 +0,0 @@
-﻿using Simple.Http.Orchestrator.Contracts;
-
-namespace Simple.Http.Orchestrator.Services;
-
-public interface IServiceOrchestrator
-{
-    Task ExecuteByOrderAsync(Activity activity, CancellationToken cancellationToken = default);
-}
