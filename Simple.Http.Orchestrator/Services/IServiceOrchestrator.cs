8a782578e9d73a24f08dc5ca6b41aedc889e67c6
Arnas Golubickas <88935098+ArgoTown@users.noreply.github.com>
2023-08-24 23:51:29 +0300

    Revert "Remove additional folders"

---
+++ Simple.Http.Orchestrator/Services/IServiceOrchestrator.cs
@@ -0,0 +1,8 @@
+﻿using Simple.Http.Orchestrator.Contracts;
+
+namespace Simple.Http.Orchestrator.Services;
+
+public interface IServiceOrchestrator
+{
+    Task ExecuteByOrderAsync(Activity activity, CancellationToken cancellationToken = default);
+}
---
+++ Simple.Http.Orchestrator/Services/ServiceOrchestrator.cs
@@ -0,0 +1,67 @@
+﻿using Simple.Http.Orchestrator.Contracts;
+
+namespace Simple.Http.Orchestrator.Services;
+
+public class ServiceOrchestrator : IServiceOrchestrator
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public ServiceOrchestrator(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task ExecuteByOrderAsync(Activity activity, CancellationToken cancellationToken = default)
+    {
+        var requestsDictionary = activity.Requests
+            .OrderBy(request => request.ExecutionOrder)
+            .ToDictionary(x => x.Id);
+
+        foreach (var (requestUniqueId, request) in requestsDictionary)
+        {
+            if (!request.Completed)
+            {
+                var dependencies = request.Dependencies.ToList();
+                if (dependencies.Any())
+                {
+                    var isMultipleDependenciesCompleted = true;
+
+                    foreach (var dependency in dependencies)
+                    {
+                        if (requestsDictionary[dependency].IsAllDependenciesCompleted)
+                        {
+                            continue;
+                        }
+
+                        isMultipleDependenciesCompleted = false;
+                    }
+
+                    if (!isMultipleDependenciesCompleted)
+                    {
+                        continue;
+                    }
+
+                    await SetRequestAsCompleted(request, cancellationToken);
+                }
+                else
+                {
+                    await SetRequestAsCompleted(request, cancellationToken);
+                }
+            }
+        }
+    }
+
+    private async Task SetRequestAsCompleted(Request request, CancellationToken cancellationToken)
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        var httpRequest = new HttpRequestMessage(HttpMethod.Post, request.Uri.AbsoluteUri);
+        var response = await httpClient.SendAsync(httpRequest, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            // Act on this request
+        }
+
+        request.IsAllDependenciesCompleted = true;
+        request.Completed = true;
+    }
+}
